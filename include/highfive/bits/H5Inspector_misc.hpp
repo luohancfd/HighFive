13e6a743df8ca2a7bfbb35edf8d4e527c4352c52
Luc Grosheintz <luc.grosheintz@gmail.com>
2023-05-03 21:28:56 +0200

    Implement flexible strings.
    This commit implements serializing `std::string` to both variable (the
    default) and fixed length HDF5 strings.

--- include/highfive/bits/H5Inspector_misc.hpp
+++ include/highfive/bits/H5Inspector_misc.hpp
@@ -1,16 +1,10 @@
-/*
- *  Copyright (c) 2022 Blue Brain Project
- *
- *  Distributed under the Boost Software License, Version 1.0.
- *    (See accompanying file LICENSE_1_0.txt or copy at
- *          http://www.boost.org/LICENSE_1_0.txt)
- *
- */
-
 #pragma once
 
 #include <type_traits>
 #include <cstring>
+#include <vector>
+#include <array>
+#include <string>
 #include <numeric>
 
 #include "../H5Reference.hpp"
@@ -27,7 +21,9 @@
 #include <Eigen/Eigen>
 #endif
 
+
 namespace HighFive {
+
 namespace details {
 
 inline bool checkDimensions(const std::vector<size_t>& dims, size_t n_dim_requested) {
@@ -259,14 +255,15 @@ struct inspector<std::string>: type_helper<std::string> {
         throw DataSpaceException("A std::string cannot be written directly.");
     }
 
-    static void serialize(const type& val, hdf5_type* m) {
-        *m = val.c_str();
+    template <class It>
+    static void serialize(const type& val, It m) {
+        (*m).assign(val.data(), val.size(), StringPadding::NullTerminated);
     }
 
-    static void unserialize(const hdf5_type* vec,
-                            const std::vector<size_t>& /* dims */,
-                            type& val) {
-        val = vec[0];
+    template <class It>
+    static void unserialize(const It& vec, const std::vector<size_t>& /* dims */, type& val) {
+        const auto& view = *vec;
+        val.assign(view.data(), view.length());
     }
 };
 
